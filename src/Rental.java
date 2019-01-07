67d450d70d8a262381eedc73dd1bb0c1e090df17
Dmytro Siliaiev <dsilyaev@gmail.com>
2019-01-07 17:59:59 +0100

    Move method - Movie.getCharge(int daysRented)

--- src/Rental.java
+++ src/Rental.java
@@ -17,29 +17,13 @@ public class Rental {
     }
 
     public double getCharge() {
-        double thisAmount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                thisAmount += 1.5;
-                if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
         // add bonus for a two day new release rental
-        if (this.getMovie().getPriceCode() == Movie.NEW_RELEASE &&
-                this.getDaysRented() > 1) {
+        if (movie.getPriceCode() == Movie.NEW_RELEASE
+                && daysRented > 1) {
             return 2;
         }
         return 1;
